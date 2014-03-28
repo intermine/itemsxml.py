f8c04bc576587c14191f36ebff4bf2e61bb224a8
Alex Kalderimis <alexkalderimis@gmail.com>
2014-03-28 20:55:09 +0000

    Prefix private methods with underscore

--- itemxml/document.py
+++ itemxml/document.py
@@ -10,7 +10,7 @@ class Factory:
     # The public API:
 
     def add(self, classnames, properties = None):
-        item = self.create_item(classnames, properties)
+        item = self._create_item(classnames, properties)
         self.items[item.get('id')] = item
         return item
 
@@ -18,7 +18,7 @@ class Factory:
         return self.items[itemid]
 
     def search(self, classname, properties):
-        for item in filter(lambda i: self.wanted(classname, properties, i), self.items.values()):
+        for item in filter(lambda i: self._wanted(classname, properties, i), self.items.values()):
             yield item
 
     def __iter__(self):
@@ -27,18 +27,18 @@ class Factory:
 
     # Private API
 
-    def get_next_id(self):
+    def _get_next_id(self):
         obj_id = self.id_counter
         self.id_counter = self.id_counter + 1
         return obj_id
 
-    def create_item(self, classnames, properties = None):
-        i = self.get_next_id()
+    def _create_item(self, classnames, properties = None):
+        i = self._get_next_id()
         item = Item(self.model, i, classnames, properties)
         item.validate()
         return item
 
-    def wanted(self, classname, properties, item):
+    def _wanted(self, classname, properties, item):
         class_ok = classname in item.classnames or any(cd.isa(classname) for cd in item.classes)
 
         item_props = item.properties.items()
